f9e52a901c90f51f1cd878a3cba8181611fc6459
Pratyush Pratik <pratyushpratik@hotmail.com>
2023-06-22 17:48:31 +0530

    Initial Commit

---
+++ DiagnalTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiagnalTest
+//
+//  Created by Pratyush Pratik Sinha on 22/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
